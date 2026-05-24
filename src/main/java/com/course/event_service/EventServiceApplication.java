11ad310618ecde4449e6efe360b4082ad88caba4
Francesco Ghisi <francescoghisi@outlook.com>
2026-05-24 16:24:34 -0300

    feat: db test and prod setup and insert test

---
+++ src/main/java/com/course/event_service/EventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.course.event_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EventServiceApplication.class, args);
+	}
+
+}
