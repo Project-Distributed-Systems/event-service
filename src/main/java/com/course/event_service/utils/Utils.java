9e3bb8f0b187c1f6f223d3013e4d99766821e2d0
Francesco Ghisi <francescoghisi@outlook.com>
2026-05-30 15:23:05 -0300

    feat: add api request exception

--- src/main/java/com/course/event_service/utils/Utils.java
+++ src/main/java/com/course/event_service/utils/Utils.java
@@ -1,6 +1,8 @@
 package com.course.event_service.utils;
 
 import com.course.event_service.entities.types.EventOutboxPayload;
+import com.course.event_service.exceptions.ApiRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import tools.jackson.core.JacksonException;
 import tools.jackson.databind.ObjectMapper;
@@ -15,7 +17,7 @@ public class Utils {
         try {
             payload = objectMapper.writeValueAsString(eventOutboxPayload);
         } catch (JacksonException e) {
-            throw new RuntimeException(e);
+            throw new ApiRequestException(HttpStatus.INTERNAL_SERVER_ERROR, "Error serializing to outbox data.");
         }
 
         return payload;
