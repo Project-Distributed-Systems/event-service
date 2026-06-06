4ba264a6d6a26bd2b404e3aa87686392c5403819
Francesco Ghisi <francescoghisi@outlook.com>
2026-06-06 12:04:24 -0300

    update event repository

---
+++ src/main/java/com/tickets/event_service/repository/EventRepository.java
@@ -0,0 +1,17 @@
+package com.tickets.event_service.repository;
+
+import com.tickets.event_service.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Modifying
+    @Query("UPDATE Event e SET e.availableQuantity = e.availableQuantity - 1 " +
+            "WHERE e.id = :id AND e.availableQuantity > 0")
+    int decrementQuantity(@Param("id") Long id);
+}
